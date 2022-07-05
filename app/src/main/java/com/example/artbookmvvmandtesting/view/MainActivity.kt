4f974677e1f66d3e719f73ec3037e6996dc27a5b
talhayi <talhayldrm23@gmail.com>
2022-07-06 01:31:12 +0300

    navigation created

--- app/src/main/java/com/example/artbookmvvmandtesting/view/MainActivity.kt
+++ app/src/main/java/com/example/artbookmvvmandtesting/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.artbookmvvmandtesting
+package com.example.artbookmvvmandtesting.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.artbookmvvmandtesting.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
