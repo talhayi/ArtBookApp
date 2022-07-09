c6363cff15e02ad0d87e4f2d42b507d618215f04
talhayi <talhayldrm23@gmail.com>
2022-07-10 01:56:51 +0300

    update ArtFragment

--- app/src/main/java/com/example/artbookmvvmandtesting/view/ArtFragment.kt
+++ app/src/main/java/com/example/artbookmvvmandtesting/view/ArtFragment.kt
@@ -3,23 +3,42 @@ package com.example.artbookmvvmandtesting.view
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.artbookmvvmandtesting.R
+import com.example.artbookmvvmandtesting.adapter.ArtRecyclerAdapter
 import com.example.artbookmvvmandtesting.databinding.FragmentArtsBinding
+import com.example.artbookmvvmandtesting.viewmodel.ArtViewModel
+import javax.inject.Inject
 
-class ArtFragment : Fragment(R.layout.fragment_arts){
+class ArtFragment @Inject constructor(
+    val artRecyclerAdapter: ArtRecyclerAdapter
+) : Fragment(R.layout.fragment_arts){
 
     private var fragmentBinding : FragmentArtsBinding? = null
+    lateinit var viewModel : ArtViewModel
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        viewModel = ViewModelProvider(requireActivity()).get(ArtViewModel::class.java)
+
         val binding = FragmentArtsBinding.bind(view)
         fragmentBinding = binding
+        subscribeToObserves()
+        binding.recylerViewArt.adapter = artRecyclerAdapter
+        binding.recylerViewArt.layoutManager = LinearLayoutManager(requireContext())
 
         binding.floatingActionButton.setOnClickListener {
             findNavController().navigate(ArtFragmentDirections.actionArtFragmentToArtDetailsFragment())
         }
+    }
+    private fun subscribeToObserves(){
+        viewModel.artList.observe(viewLifecycleOwner, Observer {
+            artRecyclerAdapter.arts = it
+        })
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/example/artbookmvvmandtesting/view/ArtFragmentFactory.kt
+++ app/src/main/java/com/example/artbookmvvmandtesting/view/ArtFragmentFactory.kt
@@ -3,15 +3,21 @@ package com.example.artbookmvvmandtesting.view
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentFactory
 import com.bumptech.glide.RequestManager
+import com.example.artbookmvvmandtesting.adapter.ArtRecyclerAdapter
+import com.example.artbookmvvmandtesting.adapter.ImageRecyclerAdapter
 import javax.inject.Inject
 
 class ArtFragmentFactory @Inject constructor(
-    private val glide: RequestManager
+    private val artRecyclerAdapter: ArtRecyclerAdapter,
+    private val glide: RequestManager,
+    private val imageRecyclerAdapter: ImageRecyclerAdapter,
 ): FragmentFactory(){
 
     override fun instantiate(classLoader: ClassLoader, className: String): Fragment {
 
         return when(className){
+            ImageApiFragment::class.java.name -> ImageApiFragment(imageRecyclerAdapter)
+            ArtFragment::class.java.name -> ArtFragment(artRecyclerAdapter)
             ArtDetailsFragment::class.java.name ->ArtDetailsFragment(glide)
             else ->super.instantiate(classLoader, className)
         }
