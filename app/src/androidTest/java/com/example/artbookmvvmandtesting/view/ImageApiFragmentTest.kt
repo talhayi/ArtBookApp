376524c3e08d7d11066d36f2ff557f7f4d000a47
talhayi <talhayldrm23@gmail.com>
2022-07-21 00:46:59 +0300

    ImageApiFragment Test Created

--- app/src/androidTest/java/com/example/artbookmvvmandtesting/view/ImageApiFragmentTest.kt
+++ app/src/androidTest/java/com/example/artbookmvvmandtesting/view/ImageApiFragmentTest.kt
@@ -4,15 +4,15 @@ import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.test.espresso.Espresso
-import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.action.ViewActions.replaceText
+import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.matcher.ViewMatchers.withId
+
 import com.example.artbookmvvmandtesting.R
+import com.example.artbookmvvmandtesting.adapter.ImageRecyclerAdapter
 import com.example.artbookmvvmandtesting.getOrAwaitValueAndroidTest
 import com.example.artbookmvvmandtesting.launchFragmentInHiltContainer
 import com.example.artbookmvvmandtesting.repo.FakeArtRepositoryTest
-import com.example.artbookmvvmandtesting.roomdb.Art
 import com.example.artbookmvvmandtesting.viewmodel.ArtViewModel
 import com.google.common.truth.Truth.assertThat
 import dagger.hilt.android.testing.HiltAndroidRule
@@ -22,14 +22,14 @@ import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.mockito.Mockito
-import org.mockito.Mockito.verify
 import javax.inject.Inject
 
 @HiltAndroidTest
 @ExperimentalCoroutinesApi
-class ArtDetailsFragmentTest {
+class ImageApiFragmentTest {
 
     private lateinit var viewModel: ArtViewModel
+    private lateinit var imageRecyclerAdapter: ImageRecyclerAdapter
 
     @get: Rule
     var hiltRule = HiltAndroidRule(this)
@@ -46,52 +46,38 @@ class ArtDetailsFragmentTest {
     }
 
     @Test
-    fun testNavigationFromArtDetailsToImageAPI(){
+    fun selectImage(){
 
         val navController = Mockito.mock(NavController::class.java)
-
-        launchFragmentInHiltContainer<ArtDetailsFragment>(
-            factory = fragmentFactory
-        ){
-            Navigation.setViewNavController(requireView(),navController)
-        }
-
-        onView(withId(R.id.artImageView)).perform(click())
-        verify(navController).navigate(
-            ArtDetailsFragmentDirections.actionArtDetailsFragmentToImageApiFragment()
-        )
-
-    }
-    @Test
-    fun testOnBackPressed(){
-
-        val navController = Mockito.mock(NavController::class.java)
-
-        launchFragmentInHiltContainer<ArtDetailsFragment>(
-            factory = fragmentFactory
-        ){
-            Navigation.setViewNavController(requireView(),navController)
-        }
-        Espresso.pressBack()
-        verify(navController).popBackStack()
-    }
-
-    @Test
-    fun testSave(){
-
+        val selectedImageUrl = "talhayildirim.com"
         val testViewModel = ArtViewModel(FakeArtRepositoryTest())
-        launchFragmentInHiltContainer<ArtDetailsFragment>(
-            factory = fragmentFactory
 
+        launchFragmentInHiltContainer<ImageApiFragment>(
+            factory = fragmentFactory
         ){
+            Navigation.setViewNavController(requireView(),navController)
             viewModel = testViewModel
+            imageRecyclerAdapter.images = listOf(selectedImageUrl)
         }
-        onView(withId(R.id.nameText)).perform(replaceText("Mona Lisa"))
-        onView(withId(R.id.artistText)).perform(replaceText("Da Vinci"))
-        onView(withId(R.id.yearText)).perform(replaceText("1500"))
-        onView(withId(R.id.saveButton)).perform(click())
-        assertThat(testViewModel.artList.getOrAwaitValueAndroidTest()).contains(
-            Art("Mona Lisa","Da Vinci",1500,"")
+        Espresso.onView(withId(R.id.imageRecyclerView)).perform(
+            RecyclerViewActions.actionOnItemAtPosition<ImageRecyclerAdapter.ImageViewHolder>(
+                0,click()
+            )
         )
+        Mockito.verify(navController).popBackStack()
+        assertThat(testViewModel.selectedImageUrl.getOrAwaitValueAndroidTest()).isEqualTo(selectedImageUrl)
+
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
